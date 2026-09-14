d27a4c37519cb09a438629416895beb063bd5f8c
88d-nz <eightyeightdee@gmail.com>
2026-09-14 19:57:56 +1200

    Fix HTML rendering - add safe filter to content variable

--- app.py
+++ app.py
@@ -232,7 +232,7 @@ TEMPLATE = '''
                 </div>
             {% endif %}
         {% endwith %}
-        {{ content }}
+        {{ content | safe }}
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
